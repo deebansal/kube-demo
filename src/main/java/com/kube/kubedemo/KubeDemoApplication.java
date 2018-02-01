6b9b7e2bad3ae6ba8d8ee022144069e856a098e7
deebansal <deepshikhabansal@gmail.com>
2018-02-01 11:07:31 -0500

    initial commit

---
+++ src/main/java/com/kube/kubedemo/KubeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.kube.kubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeDemoApplication.class, args);
+	}
+}
